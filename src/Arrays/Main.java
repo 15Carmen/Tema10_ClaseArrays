c9bf8934013dcb6d40a9692835eec0bc9efc4e67
Carmen Martín Núñez <carmen.martin@iesnervion.es>
2022-03-23 14:33:03 +0100

    commit 2

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -5,55 +5,59 @@ import java.util.Arrays;
 import static java.util.Arrays.copyOf;
 
 public class Main {
+
     public static int [] num1={1,4,8,12,45,0,7,32,62,78,18};
+
     public static void main(String[] args) {
+
         //Imprimo el array de enteros
-        //1º recorriendolo
-        //2º con el método Arrays.toString(array)
-        Metodos.String(num1);
-
-// incializo mi tabla a valor 99
-        Arrays.fill(num1,99);
-        System.out.println("inicializo mi tabla a valor 99");
         System.out.println(Arrays.toString(num1));
 
-// incializo mi tabla a valor -5 desde las posiciones 1 a la 5
-        Arrays.fill(num1,1,5,-5);
-        System.out.println("incializo mi tabla a valor -5 desde las posiciones 1 a la 5");
+        //inicializamos la tabla a 5
+
+        //forma1
+        Arrays.fill(num1,0,4,5);
+        System.out.println("\nIncializo mi tabla a valor 5 desde las posiciones 1 a la 4");
         System.out.println(Arrays.toString(num1));
+        System.out.println("");
 
-// comparo dos tablas con equals
-        int[] num2 = {99, -5, -5, -5, -5, 99, 99, 99, 99, 99, 99};
-        System.out.println("las tablas num1 y num2 son iguales? true or false?: "+ Arrays.equals(num2,num2));
+        //forma2
+        Arrays.fill(num1,5);
+        System.out.println("Incializo mi tabla a valor 5 en todas las posiciones");
+        System.out.println(Arrays.toString(num1));
+        System.out.println("");
 
-// busco un valor en una tabla ordenada
-        char[] char1 = {'a','b','c','d','e','f','g','i','j'};
-        System.out.println("la letra 'e' se encuentra en la posición "+ Arrays.binarySearch(char1,'e'));
-        System.out.println("la letra 'h' habría que insertarla en la posición "
-                + (-Arrays.binarySearch(char1,'h')-1));
+        //comparo dos tablas con equals
+        int[] num2 = {1,4,8,12,45,0,7,32,62,78,18};
+        System.out.println("La tabla 2 es:  "+Arrays.toString(num2));
+        System.out.println("¿Las tablas 1 y 2 son iguales? False=No, True=Si \n" + Arrays.equals(num1, num2));
+        System.out.println("");
 
-// busco un valor en la misma tabla pero entre un rango de índices
-        System.out.println("la letra 'j' se encuentra en la posición "+
-                Arrays.binarySearch(char1,5,9,'j'));
+        //busco un valor en una tabla ordenada
 
-// Insertar la letra 'h' en la tabla char1
-// voy a crear un nuevo array con posiciones libres y le copio el contenido de mi tabla de caracteres original
-        char[] char2=new char[char1.length +10];//creacion del nuevo array
-        char2= Arrays.copyOf(char1,char2.length); //copio el array char1 en char2
+        char[] abc = {'a','b','c','d','e','f','g'};
+        System.out.println("La letra 'd' se encuentra en la posición "+Arrays.binarySearch(abc, 'd'));
 
-// Busco la posición que debería estar. hay que diferenciar si ya se encuentra o no
-        int ultimaPosicion=char1.length-1;
-        int indicePosicion= (Arrays.binarySearch(char1, 'a'));
-            if (indicePosicion<0){
-            indicePosicion= -(indicePosicion)-1;
-        }
-// Desplazo desde indice posición hasta el final los elementos una posición para hacer hueco
-        for (int i = ultimaPosicion; i >=indicePosicion ; i--) {
-            char2[ultimaPosicion+1]=char2[ultimaPosicion];
-        }
-        char2[indicePosicion]='h';
+        //si no lo encuentra, te da la posición en la que debería estar
+        //además hay que restarle -1 a toda la sentencia, así como poner -Arrays
+        System.out.println("la letra 'h' no se encuentra en la tabla, pero debería estar en la posición  "
+                + (-Arrays.binarySearch(abc,'h')-1));
+        System.out.println("");
 
-        System.out.println(Arrays.toString(char2));
+        //busco un valor en la misma tabla pero
+        char[] abc2 = Arrays.copyOf(abc, abc.length+10);
+        System.out.println("Copia de abc en abc2 y le aumentamos 10 posiciones");
+        System.out.println(Arrays.toString(abc2));
+        abc2 = Arrays.copyOfRange(abc,0, abc2.length);
+        Arrays.fill(abc2,abc.length, abc2.length, '~');
+
+        System.out.println("En el array 1, encontramos que la letra 'h' debería estar en la posición "
+                +Arrays.binarySearch(abc, 'h'));
+        System.out.println("En el array 2, encontramso que la letra 'h' debería estar en la posición "
+                +Arrays.binarySearch(abc2, 'h'));
+
+
+
 
     }
 }
